568b73bb5cf333bacbc3021b0c910b2b6eddab2f
Nate Fischer <ntfschr@gmail.com>
2016-02-18 00:31:43 -0800

    Adding in ohm module

---
+++ node_modules/ohm-js/dist/built-in-rules.js
@@ -0,0 +1,16 @@
+var ohm = require('..');
+module.exports = ohm.makeRecipe(function() {
+  return new this.newGrammar("BuiltInRules")
+    .define("alnum", [], this.alt(this.app("letter"), this.app("digit")), "an alpha-numeric character")
+    .define("letter", [], this.alt(this.app("lower"), this.app("upper"), this.app("unicodeLtmo")), "a letter")
+    .define("digit", [], this.range("0", "9"), "a digit")
+    .define("hexDigit", [], this.alt(this.app("digit"), this.range("a", "f"), this.range("A", "F")), "a hexadecimal digit")
+    .define("ListOf_some", ["elem", "sep"], this.seq(this.param(0), this.star(this.seq(this.param(1), this.param(0)))))
+    .define("ListOf_none", ["elem", "sep"], this.seq())
+    .define("ListOf", ["elem", "sep"], this.alt(this.app("ListOf_some", [this.app("elem"), this.app("sep")]), this.app("ListOf_none", [this.app("elem"), this.app("sep")])))
+    .define("listOf_some", ["elem", "sep"], this.seq(this.param(0), this.star(this.seq(this.param(1), this.param(0)))))
+    .define("listOf_none", ["elem", "sep"], this.seq())
+    .define("listOf", ["elem", "sep"], this.alt(this.app("listOf_some", [this.app("elem"), this.app("sep")]), this.app("listOf_none", [this.app("elem"), this.app("sep")])))
+    .build();
+});
+
---
+++ node_modules/ohm-js/src/nodes.js
@@ -0,0 +1,110 @@
+'use strict';
+
+var inherits = require('inherits');
+
+// --------------------------------------------------------------------
+// Private stuff
+// --------------------------------------------------------------------
+
+function Node(grammar, ctorName, children, interval) {
+  this.grammar = grammar;
+  this.ctorName = ctorName;
+  this.children = children;
+  this.interval = interval;
+}
+
+Node.prototype.numChildren = function() {
+  return this.children.length;
+};
+
+Node.prototype.childAt = function(idx) {
+  return this.children[idx];
+};
+
+Node.prototype.indexOfChild = function(arg) {
+  return this.children.indexOf(arg);
+};
+
+Node.prototype.hasChildren = function() {
+  return this.children.length > 0;
+};
+
+Node.prototype.hasNoChildren = function() {
+  return !this.hasChildren();
+};
+
+Node.prototype.onlyChild = function() {
+  if (this.children.length !== 1) {
+    throw new Error(
+        'cannot get only child of a node of type ' + this.ctorName +
+        ' (it has ' + this.numChildren() + ' children)');
+  } else {
+    return this.firstChild();
+  }
+};
+
+Node.prototype.firstChild = function() {
+  if (this.hasNoChildren()) {
+    throw new Error(
+        'cannot get first child of a ' + this.ctorName + ' node, which has no children');
+  } else {
+    return this.childAt(0);
+  }
+};
+
+Node.prototype.lastChild = function() {
+  if (this.hasNoChildren()) {
+    throw new Error(
+        'cannot get last child of a ' + this.ctorName + ' node, which has no children');
+  } else {
+    return this.childAt(this.numChildren() - 1);
+  }
+};
+
+Node.prototype.childBefore = function(child) {
+  var childIdx = this.indexOfChild(child);
+  if (childIdx < 0) {
+    throw new Error('Node.childBefore() called w/ an argument that is not a child');
+  } else if (childIdx === 0) {
+    throw new Error('cannot get child before first child');
+  } else {
+    return this.childAt(childIdx - 1);
+  }
+};
+
+Node.prototype.childAfter = function(child) {
+  var childIdx = this.indexOfChild(child);
+  if (childIdx < 0) {
+    throw new Error('Node.childAfter() called w/ an argument that is not a child');
+  } else if (childIdx === this.numChildren() - 1) {
+    throw new Error('cannot get child after last child');
+  } else {
+    return this.childAt(childIdx + 1);
+  }
+};
+
+Node.prototype.isTerminal = function() {
+  return false;
+};
+
+Node.prototype.toJSON = function() {
+  var r = {};
+  r[this.ctorName] = this.children;
+  return r;
+};
+
+function TerminalNode(grammar, value, interval) {
+  Node.call(this, grammar, '_terminal', [], interval);
+  this.primitiveValue = value;
+}
+inherits(TerminalNode, Node);
+
+TerminalNode.prototype.isTerminal = function() {
+  return true;
+};
+
+// --------------------------------------------------------------------
+// Exports
+// --------------------------------------------------------------------
+
+module.exports = {Node: Node, TerminalNode: TerminalNode};
