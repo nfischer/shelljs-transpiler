e760935704798461c63440ce29fb07fb8c983afb
Nate Fischer <ntfschr@gmail.com>
2016-03-14 20:17:08 -0700

    Ohm-build is a separate dependency now

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ require('shelljs/global');
 
 gulp.task('default', function() {
   exec('node ' + [
-      path.join(__dirname, 'scripts/ohm-build.js'),
+      path.join(__dirname, 'node_modules', 'ohm-build', 'ohm-build.js'),
       path.join(__dirname, 'src/index.generator.html'),
       path.join(__dirname, 'index.html')
     ].join(' '));
