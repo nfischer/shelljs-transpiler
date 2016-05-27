2178e24ecd7edf69f124e6907da82b3934358398
Nate Fischer <ntfschr@gmail.com>
2016-05-26 17:26:09 -0700

    Modify dev scripts

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ require('shelljs/global');
 
 gulp.task('default', function() {
   exec('node ' + [
-      path.join(__dirname, 'node_modules', 'ohm-builder', 'ohm-builder.js'),
+      path.join(__dirname, 'node_modules', '.bin', 'ohm-builder.js'),
       path.join(__dirname, 'src/index.generator.html'),
       path.join(__dirname, 'index.html')
     ].join(' '));
